d3bddcc36f64c56f3c84abf89f31b6afa6f39538
/apn <apn@giosg.com>
2020-12-04 12:40:48 +0200

    initial version

---
+++ app/src/main/java/com/example/giosgwebviewdemo/FirstFragment.java
@@ -0,0 +1,63 @@
+package com.example.giosgwebviewdemo;
+
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class FirstFragment extends Fragment {
+    private  WebView webView;
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+        webView =(WebView)view.findViewById(R.id.firstWebview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAllowContentAccess(true);
+        webView.getSettings().setAllowFileAccessFromFileURLs(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        //URL "....."
+        //webView.loadUrl("file:///android_assets/interaction.txt");
+        try {
+            AssetManager assetManager = getContext().getAssets();
+            InputStream stream = assetManager.open("interaction.html");
+            BufferedReader r = new BufferedReader(new InputStreamReader(stream));
+            StringBuilder total = new StringBuilder();
+            String line;
+            while ((line = r.readLine()) != null) {
+                total.append(line).append("\n");
+            }
+            webView.loadDataWithBaseURL(null, total.toString(), "text/html", "UTF-8", null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
